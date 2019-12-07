2abd9edffe25af3226e401c6306988974ff7e50e
amitramesh99 <amitramesh99@gmail.com>
2019-12-07 18:34:40 -0500

    Adds memory manager

--- StudentManager/src/MemoryManager.java
+++ StudentManager/src/MemoryManager.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 // On my honor:
 //
 // - I have not used source code obtained from another student,
@@ -28,9 +30,78 @@
  * @version 2019.12.09
  */
 public class MemoryManager {
-    
+    private ArrayList<MemoryHandle> freeList;
+    private int numBytes;
     
     public MemoryManager() {
+        freeList = new ArrayList<MemoryHandle>();
+        numBytes = 0;
+    }
+    
+    public MemoryHandle getBlock(int length) {
+        for(int i=0; i<freeList.size(); i++) {
+            int blockLength = freeList.get(i).getLength();
+            if(blockLength >= length) {
+                MemoryHandle handle = new MemoryHandle(freeList.get(i).getStart(), length);
+                // subtract size
+                freeList.get(i).setLength(blockLength - length);
+                freeList.get(i).setStart(freeList.get(i).getStart() + length);
+                
+                if(freeList.get(i).getLength() <= 0) {
+                    freeList.remove(i);
+                }
+                
+                return handle;
+            }
+        }
+        
+        // create new block at end
+        MemoryHandle handle = new MemoryHandle(numBytes, length);
+        numBytes += length;
+        
+        return handle;
+        
+    }
+    
+    public void removeBlock(MemoryHandle handle) {
+        
+        // find first free block past handle
+        int i = 0;
+        while(i<freeList.size() && freeList.get(i).getStart() < handle.getStart()) {
+            i++;
+        }
+        freeList.add(i, handle);
+        
+        if(i >= 1) {
+//          check for overlap
+            MemoryHandle prev = freeList.get(i-1);
+            MemoryHandle curr = freeList.get(i);
+            if(prev.getStart() + prev.getLength() >= curr.getStart()) {            
+//                merge prev and i
+                prev.setLength(curr.getLength() + curr.getStart() - prev.getStart());
+                freeList.remove(i);
+                i--;
+            }
+        }
+        
+        if(i+1 < freeList.size()) {
+            MemoryHandle curr = freeList.get(i);
+            MemoryHandle next = freeList.get(i+1);
+            if(curr.getStart() + curr.getLength() >= next.getStart()) {
+//                merge curr and i+1
+                curr.setLength(next.getLength() + next.getStart() - curr.getStart());
+                freeList.remove(i+1);
+            }
+        }
+        
+        
+        //if space at end remove last block
+        MemoryHandle last = freeList.get(freeList.size()-1);
+        if(last.getStart() + last.getLength() >= numBytes) {
+            numBytes -= last.getLength();
+            freeList.remove(freeList.size()-1);
+        }
+        
         
     }
 
---
+++ StudentManager/src/MemoryManagerTest.java
@@ -0,0 +1,55 @@
+
+public class MemoryManagerTest extends student.TestCase{
+    private MemoryManager manager;
+    
+    public void setUp() {
+        manager = new MemoryManager();
+    }
+    
+    public void testGetBlock() {
+        MemoryHandle handle1 = manager.getBlock(10);
+        assertEquals(handle1.getLength(), 10);
+        assertEquals(handle1.getStart(), 0);
+        
+        MemoryHandle handle2 = manager.getBlock(20);
+        assertEquals(handle2.getLength(), 20);
+        assertEquals(handle2.getStart(), 10);
+        
+        MemoryHandle handle3 = manager.getBlock(30);
+        assertEquals(handle3.getLength(), 30);
+        assertEquals(handle3.getStart(), 30);
+        
+        manager.removeBlock(handle3);
+        manager.removeBlock(handle1);
+        
+        MemoryHandle handle4 = manager.getBlock(40);
+        assertEquals(handle4.getLength(), 40);
+        assertEquals(handle4.getStart(), 30);
+        
+        MemoryHandle handle5 = manager.getBlock(5);
+        assertEquals(handle5.getLength(), 5);
+        assertEquals(handle5.getStart(), 0);
+        
+        MemoryHandle handle6 = manager.getBlock(5);
+        assertEquals(handle6.getLength(), 5);
+        assertEquals(handle6.getStart(), 5);
+    }
+    
+    public void testRemoveBlock() {
+        MemoryHandle handle1 = manager.getBlock(10);
+        MemoryHandle handle2 = manager.getBlock(20);
+        MemoryHandle handle3 = manager.getBlock(30);
+        MemoryHandle handle4 = manager.getBlock(10);
+        
+        manager.removeBlock(handle1);
+        manager.removeBlock(handle3);
+        manager.removeBlock(handle2);
+        
+        MemoryHandle handle5 = manager.getBlock(50);
+        assertEquals(handle5.getLength(), 50);
+        assertEquals(handle5.getStart(), 0);
+        
+        
+        
+    }
+}
